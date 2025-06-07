20644ea77f688e7143a211b7377b595cec40ca92
dishantsethi <dishantsethi14@gmail.com>
2025-06-07 21:02:55 +0530

    update auditlog

--- auditlog/admin.py
+++ auditlog/admin.py
@@ -1,53 +1,28 @@
-from functools import cached_property
-
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
-from auditlog.filters import ResourceTypeFilter
 from auditlog.mixins import LogEntryAdminMixin
 from auditlog.models import LogEntry
 
 
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin, LogEntryAdminMixin):
-    list_select_related = ["content_type"]
     list_display = [
-        "created",
-        "resource_url",
-        "action",
-        "msg_short",
-        "user_url",
+        "source",
+        "change_value",
+        "timestamp"
     ]
     search_fields = [
-        "timestamp",
-        "object_representation",
+        "source",
         "change_value",
     ]
-    list_filter = ["action", ResourceTypeFilter]
-    readonly_fields = ["created", "resource_url", "action", "user_url", "msg"]
-    fieldsets = [
-        (None, {"fields": ["created", "user_url", "resource_url"]}),
-        (_("change_value"), {"fields": ["action", "msg"]}),
-    ]
+    
 
     def has_add_permission(self, request):
         return False
 
     def has_change_permission(self, request, obj=None):
-        return False
-
-    @cached_property
-    def _own_url_names(self):
-        return [pattern.name for pattern in self.urls if pattern.name]
-
-    def has_delete_permission(self, request, obj=None):
-        if (
-            request.resolver_match
-            and request.resolver_match.url_name not in self._own_url_names
-        ):
-            # only allow cascade delete to satisfy delete_related flag
-            return super().has_delete_permission(request, obj)
         return False
 
     def get_queryset(self, request):
--- auditlog/middleware.py
+++ auditlog/middleware.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
+from auditlog.context import set_source
+from django.utils.functional import SimpleLazyObject
+from django.conf import settings
 
+from typing import Optional
 
 class AuditlogMiddleware:
     """
@@ -9,6 +13,47 @@ class AuditlogMiddleware:
 
     def __init__(self, get_response=None):
         self.get_response = get_response
+    
+    @staticmethod
+    def _get_remote_addr(request):
+        if settings.AUDITLOG_DISABLE_REMOTE_ADDR:
+            return None
 
+        # In case there is no proxy, return the original address
+        if not request.headers.get("X-Forwarded-For"):
+            return request.META.get("REMOTE_ADDR")
+
+        # In case of proxy, set 'original' address
+        remote_addr: str = request.headers.get("X-Forwarded-For").split(",")[0]
+
+        # Remove port number from remote_addr
+        if "." in remote_addr and ":" in remote_addr:  # IPv4 with port (`x.x.x.x:x`)
+            remote_addr = remote_addr.split(":")[0]
+        elif "[" in remote_addr:  # IPv6 with port (`[:::]:x`)
+            remote_addr = remote_addr[1:].split("]")[0]
+
+        return remote_addr
+    
+    @staticmethod
+    def _get_remote_port(request) -> Optional[int]:
+        remote_port = request.headers.get("X-Forwarded-Port", "")
+
+        try:
+            remote_port = int(remote_port)
+        except ValueError:
+            remote_port = None
+
+        return remote_port
+    
     def __call__(self, request):
-        return self.get_response(request)
+        remote_addr = self._get_remote_addr(request)
+        remote_port = self._get_remote_port(request)
+
+        def get_user():
+            user = getattr(request, "user", None)
+            if callable(user):
+                user = user()
+            return user if isinstance(user, get_user_model()) and user.is_authenticated else None
+
+        with set_source(source=SimpleLazyObject(get_user), remote_addr=remote_addr, remote_port=remote_port):
+            return self.get_response(request)
